0ca34bd244f32708e9cf886731e0da004618fd33
Siddharthirkal <104129670+Siddharthirkal@users.noreply.github.com>
2026-03-22 13:57:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 //your JS code here. If required.
+const form = document.getElementById("loginForm");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const existingBtn = document.getElementById("existing");
+
+window.onload = function() {
+	const savedUser = localStorage.getItem("username");
+	const savedPass = localStorage.getItem("password");
+	if(savedUser && savedPass){
+		existingBtn.style.display="block";
+	}
+};
+
+form.addEventListener("submit",function(e){
+	e.preventDefault();
+	const user = username.value;
+	const pass = password.value;
+
+	alert("Logged in as" + user){
+		localStorage.setItem("username",user);
+		localStorage.setItem("password",pass);
+	}
+	else{
+		localStorage.removeItem("username");
+		localStorage.removeItem("password");
+	}
+});
+
+existingBtn.addEventListener("click",function(){
+	const savedUser = localStorage.getItem("username");
+	if(savedUser){
+		alert("Logged in as " + savedUser);
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
